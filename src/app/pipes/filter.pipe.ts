1f675631af251827738b41d69ec9dd18fc65f19c
rubenaraus <ing_ruben_araus@hotmail.es>
2019-12-24 15:14:08 -0600

    feature: fix buscador por nombre

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -1,16 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Tramite } from '../clases/tramite';
 
 @Pipe({
   name: 'filterByName'
 })
 export class FilterPipe implements PipeTransform {
 
-    transform(items: any, termino: string): any {
+    transform(items: Array<Tramite>, termino: string): any {
         if(!items) return [];
         if(!termino) return items;
 
         termino = termino.toLowerCase();
-        return items.filter(item => item.nombre.toLowerCase().includes(termino));
+        // return items.filter(item => item.nombre.toLowerCase().includes(termino));
+        
+        return items.filter((item: Tramite) => (item.nombre) ? item.nombre.toLowerCase().includes(termino) : false );
     }
 
 }
